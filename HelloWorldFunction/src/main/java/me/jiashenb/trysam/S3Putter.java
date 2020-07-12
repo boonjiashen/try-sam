cf9455fae3b442918e7bb0ffe4696e4513fdbef2
Jia-Shen Boon <jiashen@gmail.com>
2020-07-12 09:38:26 +0900

    Add mockito

--- HelloWorldFunction/src/main/java/me/jiashenb/trysam/S3Putter.java
+++ HelloWorldFunction/src/main/java/me/jiashenb/trysam/S3Putter.java
@@ -5,10 +5,21 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 public class S3Putter implements RequestHandler<String, Void> {
+
+    private final DynamoDbClient client;
+
+    public S3Putter(DynamoDbClient client) {
+        this.client = client;
+    }
+
+    public S3Putter() {
+        this.client = DynamoDbClient.builder().build();
+    }
+
     @Override
     public Void handleRequest(String s, Context context) {
-        DynamoDbClient.builder().build().listTables().tableNames().forEach(name -> System.out.println(name));
-        System.out.println("Hello in S3Putter!");
+        client.listTables().tableNames().forEach(name -> System.out.println(name));
+        System.out.println("Hello in S3Putter v2!");
 
         return null;
     }
--- HelloWorldFunction/src/test/java/me/jiashenb/trysam/S3PutterTest.java
+++ HelloWorldFunction/src/test/java/me/jiashenb/trysam/S3PutterTest.java
@@ -1,11 +1,32 @@
 package me.jiashenb.trysam;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class S3PutterTest {
+
+    @Mock
+    private static DynamoDbClient client;
+
+    private S3Putter putter;
+
+    @BeforeEach
+    public void setup() {
+        putter = new S3Putter(client);
+    }
 
     @Test
     public void shouldNotThrow() {
-        new S3Putter().handleRequest("", null);
+        when(client.listTables()).thenReturn(ListTablesResponse.builder().build());
+
+        putter.handleRequest("", null);
     }
 }
