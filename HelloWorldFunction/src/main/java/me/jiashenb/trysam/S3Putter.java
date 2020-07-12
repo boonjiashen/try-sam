d953f5e8269f8593012bf3d4a5f31c69d152cc54
Jia-Shen Boon <jiashen@gmail.com>
2020-07-12 15:18:14 +0900

    Make S3Putter put multiple files asynchronously
    To try X-ray's behaviour on multi-threaded applications

--- HelloWorldFunction/src/main/java/me/jiashenb/trysam/S3Putter.java
+++ HelloWorldFunction/src/main/java/me/jiashenb/trysam/S3Putter.java
@@ -2,11 +2,20 @@ package me.jiashenb.trysam;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.core.client.config.SdkAdvancedAsyncClientOption;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class S3Putter implements RequestHandler<String, Void> {
 
@@ -14,28 +23,44 @@ public class S3Putter implements RequestHandler<String, Void> {
 
     private static final String BUCKET_NAME = System.getenv(BUCKET_NAME_ENVVAR);
 
-    private final S3Client client;
+    private static final int NUM_OBJECTS_TO_PUT = 100;
 
-    public S3Putter(S3Client client) {
-        this.client = client;
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(5);
+
+    private final S3AsyncClient s3AsyncClient;
+
+    public S3Putter(S3AsyncClient s3AsyncClient) {
+        this.s3AsyncClient = s3AsyncClient;
     }
 
     public S3Putter() {
-        this.client = S3Client.builder().build();
+        this.s3AsyncClient = S3AsyncClient.builder()
+                .asyncConfiguration(b -> b.advancedOption(SdkAdvancedAsyncClientOption.FUTURE_COMPLETION_EXECUTOR
+                , EXECUTOR_SERVICE))
+                .build();
     }
 
     @Override
     public Void handleRequest(String s, Context context) {
-        String s3Key = getEpochString();
-        PutObjectRequest request = PutObjectRequest.builder()
-                .bucket(BUCKET_NAME)
-                .key(s3Key)
-                .build();
-        client.putObject(request, RequestBody.empty());
-        System.out.println("Put object in bucket " + BUCKET_NAME + " and key " + s3Key);
-        System.out.println("Hello in S3Putter v2!");
+        String s3Folder = getEpochString();
+        List<CompletableFuture<PutObjectResponse>> futures = Stream.generate(this::generateFilename)
+                .limit(NUM_OBJECTS_TO_PUT)
+                .map(filename -> PutObjectRequest
+                        .builder()
+                        .bucket(BUCKET_NAME)
+                        .key(s3Folder + "/" + filename)
+                        .build())
+                .map(request ->
+                        s3AsyncClient.putObject(request, AsyncRequestBody.empty())
+                )
+                .collect(Collectors.toList());
+        futures.forEach(CompletableFuture::join);
 
         return null;
+    }
+
+    private String generateFilename() {
+        return getEpochString() + "-" + UUID.randomUUID().toString();
     }
 
     private String getEpochString() {
--- HelloWorldFunction/src/test/java/me/jiashenb/trysam/S3PutterTest.java
+++ HelloWorldFunction/src/test/java/me/jiashenb/trysam/S3PutterTest.java
@@ -5,13 +5,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class S3PutterTest {
 
     @Mock
-    private static S3Client client;
+    private static S3AsyncClient client;
 
     private S3Putter putter;
 
@@ -22,6 +29,8 @@ public class S3PutterTest {
 
     @Test
     public void shouldNotThrow() {
+        when(client.putObject(any(PutObjectRequest.class), any(AsyncRequestBody.class))).thenReturn(CompletableFuture.completedFuture(null));
+
         putter.handleRequest("", null);
     }
 }
