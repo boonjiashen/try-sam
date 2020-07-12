d38eaffe534ca3337ec281e4106fa28b2bf53284
Jia-Shen Boon <jiashen@gmail.com>
2020-07-12 16:35:41 +0900

    Add put functionality to DynamoDB putter

--- HelloWorldFunction/src/main/java/me/jiashenb/trysam/DdbPutter.java
+++ HelloWorldFunction/src/main/java/me/jiashenb/trysam/DdbPutter.java
@@ -2,17 +2,58 @@ package me.jiashenb.trysam;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.util.UUID;
 
 public class DdbPutter implements RequestHandler<String, Void> {
 
-    private static final String TABLE_NAME_ENVVAR = "TABLE_NAME";
+    public static final String TABLE_NAME_ENVVAR = "TABLE_NAME";
 
     private static final String TABLE_NAME = System.getenv(TABLE_NAME_ENVVAR);
+
+    private final DynamoDbTable<Id> table;
+
+    public DdbPutter() {
+        DynamoDbClient ddbClient = DynamoDbClient.builder().build();
+        DynamoDbEnhancedClient ddbEnhancedClient = DynamoDbEnhancedClient.builder().dynamoDbClient(ddbClient).build();
+        this.table = ddbEnhancedClient.table(TABLE_NAME, TableSchema.fromBean(Id.class));
+    }
+
+    public DdbPutter(DynamoDbTable<Id> table) {
+        this.table = table;
+    }
 
     @Override
     public Void handleRequest(String s, Context context) {
         System.out.println(TABLE_NAME);
 
+        Id item = new Id();
+        String id = UUID.randomUUID().toString();
+        item.setId(id);
+        table.putItem(item);
+
+        System.out.println("ID = " + id);
+
         return null;
+    }
+
+    @DynamoDbBean
+    public static final class Id {
+        private String id;
+
+        @DynamoDbPartitionKey
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
     }
 }
--- HelloWorldFunction/src/test/java/me/jiashenb/trysam/DdbPutterTest.java
+++ HelloWorldFunction/src/test/java/me/jiashenb/trysam/DdbPutterTest.java
@@ -1,11 +1,33 @@
 package me.jiashenb.trysam;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 class DdbPutterTest {
 
+    @Mock
+    DynamoDbTable table;
+
+    private DdbPutter putter;
+
     @Test
-    public void shouldNotThrow() {
-        new DdbPutter().handleRequest(null, null);
+    public void shouldNotThrow() throws Exception {
+        putter = new DdbPutter(table);
+        putter.handleRequest("", null);
+    }
+
+    @Test
+    public void idShouldSet() {
+        DdbPutter.Id id = new DdbPutter.Id();
+        String expectedId = "hi";
+        id.setId(expectedId);
+
+        assertEquals(id.getId(), expectedId);
     }
 }
